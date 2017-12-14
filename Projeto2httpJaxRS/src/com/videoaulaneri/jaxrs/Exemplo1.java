ba7b11b3bb66668afb8e407926203830d507bb94
JFerreira <jonasf49@gmail.com>
2017-12-14 16:59:11 -0300

    Pequenas alterações na continuidade do projeto

--- Projeto2httpJaxRS/src/com/videoaulaneri/jaxrs/Exemplo1.java
+++ Projeto2httpJaxRS/src/com/videoaulaneri/jaxrs/Exemplo1.java
@@ -1,5 +1,15 @@
 package com.videoaulaneri.jaxrs;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/exemplosrs")
 public class Exemplo1 {
+	
+	@GET
+	public String mostra() {
+		
+		return "Videoaulas Neri, executou um GET";
+	}
 
 }
